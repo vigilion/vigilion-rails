8e59f121b07e848d68fe4c2267717402071c32fa
Emiliano Ritiro <eritiro@gmail.com>
2015-05-27 21:37:07 -0300

    New way of defining columns to be scanned

--- lib/vigilion-rails.rb
+++ lib/vigilion-rails.rb
@@ -1,4 +1,22 @@
 require "vigilion-rails/engine"
 
 module VigilionRails
+
+  module ActiveRecord
+    def scan_file column, options={}
+      default = { scan_column: "#{column}_scan_status" }
+      options = default.merge(options)
+
+      class_eval <<-RUBY, __FILE__, __LINE__+1
+
+        # carrierwave callback
+        def store_#{column}!
+          #{options[:scan_column]} = 'pending'
+          ::VirusScanner::File.scan_url(id, #{column}.url)
+        end
+      RUBY
+    end
+  end
 end
+
+ActiveRecord::Base.extend VigilionRails::ActiveRecord
