affc78c2c9819c3fc6773e90c01828547d7b8bff
Emiliano Ritiro <eritiro@gmail.com>
2015-05-28 13:30:17 -0300

    removed unused migration

--- app/controllers/vigilion/vigilion_controller.rb
+++ app/controllers/vigilion/vigilion_controller.rb
@@ -1,13 +1,9 @@
 class Vigilion::VigilionController < ActionController::Base
   skip_before_filter :verify_authenticity_token
-  before_filter :get_scan_id
 
   def callback
+    @uuid = params[:uuid]
 
     render nothing: true
-  end
-
-  def get_scan_id
-    @uuid = params[:uuid]
   end
 end
