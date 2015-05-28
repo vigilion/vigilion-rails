3075f5059c937b7baff55092a0c248ec4a939652
Emiliano Ritiro <eritiro@gmail.com>
2015-05-28 13:27:39 -0300

    removed old scan model

--- app/controllers/vigilion/vigilion_controller.rb
+++ app/controllers/vigilion/vigilion_controller.rb
@@ -3,18 +3,6 @@ class Vigilion::VigilionController < ActionController::Base
   before_filter :get_scan_id
 
   def callback
-    scan = Scan.where(uuid: @uuid).first
-
-    status = case params[:status]
-    when "clean"
-      "clean"
-    when "infected"
-      "infected"
-    else
-      "unknown"
-    end
-
-    scan.update(vs_id: params[:id], status: status)
 
     render nothing: true
   end
