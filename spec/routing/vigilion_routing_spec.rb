3006b8652feb0a622f2ec7b1a9f147b4bac83b02
Emiliano Ritiro <eritiro@gmail.com>
2015-05-28 13:35:56 -0300

    fixed spec warning

--- spec/routing/vigilion_routing_spec.rb
+++ spec/routing/vigilion_routing_spec.rb
@@ -3,7 +3,7 @@ require "vigilion_rails_helper"
 describe "VigilionController" do
   describe "routing" do
     it "routes to #index" do
-      post("/vigilion/callback").should route_to("vigilion/vigilion#callback")
+      expect(post("/vigilion/callback")).to route_to("vigilion/vigilion#callback")
     end
   end
 end
