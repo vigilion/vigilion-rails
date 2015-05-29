05640f149b7714acfbf296cc7af3336c454630ef
Emiliano Ritiro <eritiro@gmail.com>
2015-05-29 02:02:10 -0300

    working (non-pretty) version

--- app/controllers/vigilion/vigilion_controller.rb
+++ app/controllers/vigilion/vigilion_controller.rb
@@ -2,8 +2,13 @@ class Vigilion::VigilionController < ActionController::Base
   skip_before_filter :verify_authenticity_token
 
   def callback
-    @uuid = params[:uuid]
-
-    render nothing: true
+    identity = JSON.parse(params[:uuid])
+    klass = identity["model"].constantize
+    model = klass.find(identity["id"])
+    mapping = klass.class_variable_get(:@@vigilion_mappings)[identity["column"]]
+    if mapping.present? && model.respond_to?(mapping)
+      model.update_attribute(mapping, params[:status])
+    end
+    head :ok
   end
 end
--- lib/vigilion-rails.rb
+++ lib/vigilion-rails.rb
@@ -4,15 +4,18 @@ module VigilionRails
 
   module ActiveRecord
     def scan_file column, options={}
-      default = { scan_column: "#{column}_scan_status" }
+      default = { scan_column: "#{column}_scan_results" }
       options = default.merge(options)
 
       class_eval <<-RUBY, __FILE__, __LINE__+1
 
+        @@vigilion_mappings ||= Hash.new
+        @@vigilion_mappings['#{column}'] = '#{options[:scan_column]}'
+
         # carrierwave callback
         def store_#{column}!
-          #{options[:scan_column]} = 'pending'
-          Vigilion::File.scan_url('FIXTHIS', 'FIXTHIS')
+          key = { model: self.class.name, column: '#{column}', id: id }.to_json
+          Vigilion::File.scan_url(key, #{column})
         end
       RUBY
     end
