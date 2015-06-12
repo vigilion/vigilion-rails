92b0cdcdb94b583e170c55b58622cb0c302d54b2
Emiliano Ritiro <eritiro@gmail.com>
2015-06-12 10:32:03 -0300

    new API endpoints

--- lib/generators/vigilion/install_generator.rb
+++ lib/generators/vigilion/install_generator.rb
@@ -2,7 +2,8 @@ class Vigilion::InstallGenerator < Rails::Generators::Base
   def create_initializer_file
     create_file "config/initializers/vigilion.rb", <<INITIALIZER
 Vigilion.configure do |config|
-  config.api_key = ENV["VIGILION_API_KEY"]
+  config.access_key_id = ENV["VIGILION_ACCESS_KEY_ID"]
+  config.secret_access_key = ENV["VIGILION_SECRET_ACCESS_KEY"]
 end
 INITIALIZER
   end
