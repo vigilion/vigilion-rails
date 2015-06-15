693242e4c192472b80e2b1571bf0d522500ba0a2
Emiliano Ritiro <eritiro@gmail.com>
2015-06-15 10:56:16 -0300

    Validates authorization

--- app/controllers/vigilion/vigilion_controller.rb
+++ app/controllers/vigilion/vigilion_controller.rb
@@ -1,5 +1,6 @@
 class Vigilion::VigilionController < ActionController::Base
   skip_before_filter :verify_authenticity_token
+  before_action :verify_api_auth
 
   def callback
     identity = JSON.parse(params[:key])
@@ -9,5 +10,16 @@ class Vigilion::VigilionController < ActionController::Base
       model.send(on_scan, params)
     end
     head :ok
+  end
+
+private
+  def verify_api_auth
+    unless digest == request.headers["Auth-Hash"]
+      render json: { message: "unauthorized" }, status: 401
+    end
+  end
+
+  def digest
+    Digest::MD5.hexdigest("#{request.raw_post}#{Vigilion::Configuration.secret_access_key}")
   end
 end
