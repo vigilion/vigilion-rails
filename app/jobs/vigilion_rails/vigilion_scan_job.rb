214489e34045241a69099824b1d2412aa466ba14
Matthew Ford <matt@bitzesty.com>
2021-08-31 12:37:58 +0100

    Lookup file on the correct classname

--- app/jobs/vigilion_rails/vigilion_scan_job.rb
+++ app/jobs/vigilion_rails/vigilion_scan_job.rb
@@ -2,8 +2,8 @@ module VigilionRails
   class VigilionScanJob < ApplicationJob
     queue_as :default
 
-    def perform(integration_class, key, column)
-      file = integration_class.constantize.find(key[:id])
+    def perform(integration_class, class_name, key, column)
+      file = class_name.constantize.find(key[:id])
 
       integration_class.constantize.new.scan key, file, column
     end
