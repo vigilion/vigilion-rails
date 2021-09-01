c52e11a0ee99f69184702bc8f0b7e3c641956b76
Matthew Ford <matt@bitzesty.com>
2021-09-01 12:59:35 +0100

    hard code the worker to use URL Scanning

--- app/jobs/vigilion_rails/vigilion_scan_job.rb
+++ app/jobs/vigilion_rails/vigilion_scan_job.rb
@@ -2,10 +2,10 @@ module VigilionRails
   class VigilionScanJob < ApplicationJob
     queue_as :default
 
-    def perform(integration_class, class_name, key, column)
+    def perform(class_name, key, column)
       file = class_name.constantize.find(key[:id])
 
-      integration_class.constantize.new.scan key, file, column
+      VigilionRails::UrlIntegration.new.scan key, file, column
     end
   end
 end
